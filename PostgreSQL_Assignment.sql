863fe7224d2e8ddf76ed8d09ab739de8b5b643ae
AALabonya <1aklimakter@gmail.com>
2024-10-22 09:43:12 -0700

    create courses table

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,5 +1,9 @@
 -- Active: 1729443357593@@127.0.0.1@5432@university_db
--- 1. Create the 'students' table
+
+
+-- 1. Table Creation:
+
+-- Create a "students" table
 CREATE TABLE students (
     student_id SERIAL PRIMARY KEY,          
     student_name VARCHAR(100) NOT NULL,              
@@ -8,4 +12,12 @@ CREATE TABLE students (
     frontend_mark INT,                      
     backend_mark INT,                       
     status VARCHAR(50)                      
+);
+
+-- Create a "courses" table
+
+REATE TABLE courses (
+    course_id SERIAL PRIMARY KEY,          
+    course_name VARCHAR(100) NOT NULL,               
+    credits INT                            
 );
