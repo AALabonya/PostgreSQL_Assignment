0a3a0d8f06c0267b845c75418a109fce681cf5b6
AALabonya <1aklimakter@gmail.com>
2024-10-22 09:40:23 -0700

    create students table

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,1 +1,11 @@
 -- Active: 1729443357593@@127.0.0.1@5432@university_db
+-- 1. Create the 'students' table
+CREATE TABLE students (
+    student_id SERIAL PRIMARY KEY,          
+    student_name VARCHAR(100) NOT NULL,              
+    age INT,                              
+    email VARCHAR(100) UNIQUE,              
+    frontend_mark INT,                      
+    backend_mark INT,                       
+    status VARCHAR(50)                      
+);
