7051c87d8671b5c28c3b7aff076e430ce23a469b
AALabonya <1aklimakter@gmail.com>
2024-10-23 01:47:10 -0700

    added readme file

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,7 +1,9 @@
 -- Active: 1729443357593@@127.0.0.1@5432@university_db
 
+--1. Crating database
+CREATE DATABASE university_db;
 
--- 1. Table Creation:
+-- 2. Table Creation:
 
 -- Create a "students" table
 CREATE TABLE students (
@@ -31,7 +33,14 @@ CREATE TABLE enrollment (
 );
 
 -- Insert data into the students table
-INSERT INTO students (student_name, age, email, frontend_mark, backend_mark, status) VALUES
+INSERT INTO 
+students ( student_name,
+           age,
+           email, 
+           frontend_mark, 
+           backend_mark, 
+           status)
+VALUES
 ('Sameer', 21, 'sameer@example.com', 48, 60, NULL),
 ('Zoya', 23, 'zoya@example.com', 52, 58, NULL),
 ('Nabil', 22, 'nabil@example.com', 37, 46, NULL),
@@ -54,24 +63,29 @@ INSERT INTO enrollment (student_id, course_id) VALUES
 (2, 1),
 (3, 2);
 
+-- SELECT * FROM students;
+-- SELECT * FROM enrollment;
+-- SELECT * FROM courses;
+
 
 -- Run SQL queries to complete the following tasks:
-Query 1:
+-- Query 1:
 -- Insert a new student record with the following details:
 
 INSERT INTO students (student_name, age, email, frontend_mark, backend_mark, status) VALUES
 ('Aklima', 25, 'aklima@example.com', 70, 75, NULL);
 
-Query 2:
+-- Query 2:
 -- Retrieve the names of all students who are enrolled in the course titled 'Next.js'.
 
 SELECT s.student_name
 FROM students s
-JOIN enrollment e ON s.student_id = e.student_id
-JOIN courses c ON e.course_id = c.course_id
-WHERE c.course_name = 'Next.js';
+JOIN enrollment e USING (student_id)
+JOIN courses c USING (course_id)
+WHERE
+    c.course_name = 'Next.js';
 
-Query 3:
+-- Query 3:
 -- Update the status of the student with the highest total (frontend_mark + backend_mark) to 'Awarded'.
 
 UPDATE students 
@@ -79,19 +93,40 @@ UPDATE students
  WHERE student_id =(
     SELECT student_id
     FROM students ORDER BY (frontend_mark + backend_mark) DESC LIMIT 1
- )
+ );
 
- Query 4:
+--  Query 4:
 -- Delete all courses that have no students enrolled.
 
 DELETE FROM courses
 WHERE course_id NOT IN(
     SELECT DISTINCT course_id
     FROM enrollment
-)
+);
 
-Query 5:
+-- Query 5:
 -- Retrieve the names of students using a limit of 2, starting from the 3rd student.
 
 SELECT student_name 
-FROM students ORDER BY student_id LIMIT 2 OFFSET 2
+FROM students ORDER BY student_id 
+LIMIT 2 
+OFFSET 2;
+
+-- Query 6:
+-- Retrieve the course names and the number of students enrolled in each course.
+SELECT c.course_name, COUNT(e.student_id) AS student_enrolled
+FROM courses c JOIN enrollment e ON c.course_id = e.course_id
+GROUP BY c.course_name
+
+-- Query 7:
+-- Calculate and display the average age of all students.
+
+SELECT AVG(age) AS average_age 
+FROM students
+
+-- Query 8:
+-- Retrieve the names of students whose email addresses contain 'example.com'
+
+SELECT student_name 
+ FROM students
+ WHERE email LIKE '%example.com%';
